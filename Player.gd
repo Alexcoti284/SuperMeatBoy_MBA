fce208403cc9eeb03da05faadbb8e549bfe31213
Marcel Lejk <marcel.lejk@educem.net>
2024-11-19 19:31:50 +0100

    modificar el player

--- Player.gd
+++ Player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0
+@export var speedFaster: float = 500.0
 @export var jump_velocity: float = -500.0
 @export var gravity: float = 1000.0
 
 var screen_size = Vector2()
+
+const FLOOR_NORMAL = Vector2.UP
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -12,19 +15,20 @@ func _ready():
 func _process(delta):
 	# Reset horizontal velocity every frame
 	velocity.x = 0
+	var current_speed = speedFaster if Input.is_action_pressed("boost") else speed
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
-		$AnimatedSprite2D.flip_h = false # Face right
+		$AnimatedSprite2D.flip_h = true # Face right
 	elif Input.is_action_pressed("move_left"):
 		velocity.x -= speed
-		$AnimatedSprite2D.flip_h = true # Face left
-
+		$AnimatedSprite2D.flip_h = false # Face left
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Gravity effect
 	velocity.y += gravity * delta
+	
 
 	# Apply movement
 	move_and_slide()
